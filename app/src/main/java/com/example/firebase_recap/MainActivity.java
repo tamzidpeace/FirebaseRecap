e70112d3fed574c639c502aaef2d3eea96c09cb1
tamzidpeace <tamjedpeace@gmail.com>
2019-03-27 23:48:11 +0600

    read information

--- app/src/main/java/com/example/firebase_recap/MainActivity.java
+++ app/src/main/java/com/example/firebase_recap/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.firebase_recap;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    //constant
+    private static final String TAG = "MainActivity";
+
+    //member variable
     private EditText editText;
     String mText;
     DatabaseReference myRef;
@@ -24,10 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
         TextView text = findViewById(R.id.textView);
         Button saveBtn = findViewById(R.id.button);
+        Button readBtn = findViewById(R.id.button2);
         editText = findViewById(R.id.editText);
 
         myRef = FirebaseDatabase.getInstance().getReference();
 
+        // inserting data
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +48,25 @@ public class MainActivity extends AppCompatActivity {
                 myRef.child("abc").child(String.valueOf(number)).setValue(mText);
             }
         });
+
+        // reading data
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //String value = dataSnapshot.getValue(String.class);
+                        String value = (String) dataSnapshot.child("abc").child("no").getValue();
+                        Log.d(TAG, "onDataChange: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.d(TAG, "onCancelled: " + databaseError.toException());
+                    }
+                });
+            }
+        });
     }
-
-
 }
